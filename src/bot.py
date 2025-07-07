b89befd676429ba055d217e4017c4064d6a853b6
Serbirial <39264658+Serbirial@users.noreply.github.com>
2025-07-07 12:14:07 -0400

    Tune to server settings

--- src/bot.py
+++ src/bot.py
@@ -101,15 +101,15 @@ class ChatBot:
         # New TinyLlama model init
         self.model = Llama(
             model_path=mainLLM,
-            n_ctx=4096,              # TODO use CTX setter 
-            n_threads=4,             # tune to setup
+            n_ctx=32768,              # TODO use CTX setter 
+            n_threads=24,             # tune to setup
             use_mlock=True,          # locks model in RAM to avoid swap on Pi (turn off if not running from a Pi)
             logits_all=False,
             verbose=False,
             use_mmap=False,
             n_gpu_layers=0,
             low_vram=True,
-            n_batch=4,
+            n_batch=500,
             numa=False
         )
 
