023c2012de77bc158d0ec814a51230ba292e71a3
Serbirial <39264658+Serbirial@users.noreply.github.com>
2025-07-12 05:49:22 -0400

    better stopping, add custom stop token

--- src/static.py
+++ src/static.py
@@ -51,6 +51,7 @@ class StopOnSpeakerChange:
         self.stopped = False
 
         self.default_stop_tokens = ["<|user|>", "<|system|>", "<|end|>", "<|eos|>", "<user>"]
+        self.hard_stop = "<force_done>"
         self.custom_stops = custom_stops or []
 
     def __call__(self, new_text_chunk):
@@ -66,7 +67,11 @@ class StopOnSpeakerChange:
             lines.append(line)
 
         all_stop_tokens = self.default_stop_tokens + self.custom_stops
-
+        if self.hard_stop in self.buffer or self.hard_stop in new_text_chunk:
+            return True
+        for token in all_stop_tokens:
+            if token in self.buffer and self.line_count >= self.min_lines:
+                return True
         for line in lines:
             print(f"Processing line: {repr(line)} | line_count: {self.line_count}")
 
